8143e843462e127242862b201fa7958cd8052560
Alejandro Prado <isc.alejandro.prado@gmail.com>
2025-09-21 13:20:52 -0600

    Add hero

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        blanka: ['Blanka', 'sans-serif'],
+        schibsted: ['Schibsted Grotesk', 'sans-serif'],
+        orbitron: ['Orbitron', 'sans-serif'],
+        bungee: ['Bungee', 'cursive'],
+        luckiest: ['Luckiest Guy', 'cursive'],
+        syncopate: ['Syncopate', 'sans-serif'],
+        chakra: ['Chakra Petch', 'sans-serif'],
+      },
+      colors: {
+        'deep-purple': '#1a1a2e',
+      },
+    },
+  },
+  plugins: [],
+}

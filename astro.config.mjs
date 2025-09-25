18da7c13836de4cd9e4381d04ef8e35974e8d7a0
Alejandro Prado <isc.alejandro.prado@gmail.com>
2025-09-24 22:41:48 -0600

    feat: configure GitHub Pages deployment with custom domain and update party details

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,10 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mikumple.com.mx',
   integrations: [tailwind()],
   server: {
     host: true, // listen on all interfaces so you can tunnel with ngrok
     port: 4321,
-  },
-  vite: {
-    server: {
-      host: true,
-      cors: true,
-      allowedHosts: ['012910788280.ngrok-free.app'],
-    },
-    preview: {
-      host: true,
-      cors: true,
-      allowedHosts: ['012910788280.ngrok-free.app'],
-    },
   },
 });
